31a9a90fb3f653afe0801b2abda8a1cd434f3ad4
overlookmotel <theoverlookmotel@gmail.com>
2019-05-25 19:45:15 +0100

    Tests: Overlook class tests reduce repetition [refactor]

--- test/overlook.test.js
+++ test/overlook.test.js
@@ -18,13 +18,17 @@ const spy = jest.fn;
 // Tests
 
 describe('Overlook class', () => { // eslint-disable-line jest/lowercase-name
+	let overlook;
+	beforeEach(() => {
+		overlook = new Overlook();
+	});
+
 	it('is a class', () => {
 		expect(Overlook).toBeFunction();
 	});
 
 	describe('constructor', () => {
 		it('initializes undefined parent', () => {
-			const overlook = new Overlook();
 			expect(overlook).toHaveProperty('router');
 			expect(overlook.router).toBeUndefined();
 		});
@@ -32,18 +36,15 @@ describe('Overlook class', () => { // eslint-disable-line jest/lowercase-name
 
 	describe('`.attachRouter()`', () => {
 		it('prototype method exists', () => {
-			const overlook = new Overlook();
 			expect(overlook.attachRouter).toBeFunction();
 		});
 
 		it('returns overlook instance for chaining', () => {
-			const overlook = new Overlook();
 			const ret = overlook.attachRouter(new Route());
 			expect(ret).toBe(overlook);
 		});
 
 		it('records router as `overlook.router`', () => {
-			const overlook = new Overlook();
 			const router = new Route();
 			overlook.attachRouter(router);
 			expect(overlook.router).toBe(router);
@@ -52,18 +53,15 @@ describe('Overlook class', () => { // eslint-disable-line jest/lowercase-name
 
 	describe('`.handle()`', () => {
 		it('prototype method exists', () => {
-			const overlook = new Overlook();
 			expect(overlook.handle).toBeFunction();
 		});
 
 		it('returns null if no router attached', () => {
-			const overlook = new Overlook();
 			const ret = overlook.handle();
 			expect(ret).toBeNull();
 		});
 
 		it('calls `router.handle()`', () => {
-			const overlook = new Overlook();
 			const router = new Route();
 			router.handle = spy();
 			overlook.attachRouter(router);
@@ -73,7 +71,6 @@ describe('Overlook class', () => { // eslint-disable-line jest/lowercase-name
 		});
 
 		it('returns result of `router.handle()`', () => {
-			const overlook = new Overlook();
 			const router = new Route();
 			const res = {};
 			router.handle = () => res;
